a6fa4fc781d1bd6f76ee06bd42ac550dbab7552e
sassnoe <105166994+sassnoe@users.noreply.github.com>
2023-03-28 10:40:15 +0200

    update

--- pokémon.js
+++ pokémon.js
@@ -4,7 +4,7 @@ window.addEventListener("load", start);
 
 function start() {
   console.log("start running");
-  const umbreon = getPokémon("data/umbreon.json");
+  const umbreon = getPokémon("https://cederdorff.github.io/dat-js/05-data/pokemons.json");
 
   showPokémon(umbreon);
 }
@@ -16,36 +16,13 @@ async function getPokémon(url) {
 }
 
 function showPokémon(pokémon) {
-  document.querySelector("body").insertAdjacentHTML(
+  document.querySelector("pokémon").insertAdjacentHTML(
     "beforeend",
     /*html*/ `
     
     <article>
         <h2>${pokémon.name}</h2>
-        <img src="${pokémon.image}"/>
+        <img src = ${pokémon.image}/>
     </article>`
   );
 }
-/* Data Structure
-name: tekst
-description: tekst
-ability: tekst
-image: url
-footprint: url (til et andet billede)
-dexindex: tal
-type: tekst – begrænset til: fire, ice, flying, etc …
-subtype: tekst
-weaknesses: tekst – en kommasepareret liste over types
-gender: tekst: male eller female
-weight: tal – vægt i gram
-height: tal – højde i cm
-generation: tal
-spilversion: tekst
-canEvolve: boolean
-statsHP: tal 0-10
-statsAttack: tal 0-10
-statsDefence: tal 0-10
-statsSpecialAttack: tal 0-10
-statsSpecialDefence: tal 0-10
-statsSpeed: 0-10
-*/
